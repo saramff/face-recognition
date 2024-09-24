be6fa41c24279c1d462fcb9f51e0fd5cc3f86b7e
Sara Martín <saramartinfuentes@hotmail.com>
2024-09-24 21:37:53 +0200

    changes

--- main.js
+++ main.js
@@ -1,46 +1,111 @@
 /**
  * Sara's Experiment
- * 
  * Some info...
  *
  */
 
-import { shuffle } from "./utils.js";
+const PEOPLE_URL =
+  "https://raw.githubusercontent.com/Amaza-ing/images/refs/heads/master/people";
+const IMAGES_PER_GENDER = 10;
 
-const PEOPLE_URL = "https://raw.githubusercontent.com/saramff/face-recognition-images/refs/heads/master/";
-const IMAGES_PER_GENDER = 5;
+// Create arrays from picture 1 to IMAGES_PER_GENDER
+const menImages = Array.from(
+  { length: IMAGES_PER_GENDER },
+  (_, i) => `${PEOPLE_URL}/men/man-${i + 1}.PNG`
+);
+const womenImages = Array.from(
+  { length: IMAGES_PER_GENDER },
+  (_, i) => `${PEOPLE_URL}/women/woman-${i + 1}.PNG`
+);
+const peopleImages = [...menImages, ...womenImages];
 
-const personImg = document.querySelector("#person-img");
+/* initialize jsPsych */
+var jsPsych = initJsPsych({
+  on_finish: function () {
+    jsPsych.data.displayData();
+  },
+});
 
-let imgSrc = "";
+/* create timeline */
+var timeline = [];
 
-// Create numeric arrays from 1 to IMAGES_PER_GENDER number and then shuffles them
-const menArray = Array.from({ length: IMAGES_PER_GENDER }, (_, i) => i + 1);
-const womenArray = Array.from({ length: IMAGES_PER_GENDER }, (_, i) => i + 1);
-// console.log("men", menArray);
-// console.log("women", womenArray);
-shuffle(menArray);
-shuffle(womenArray);
-// console.log("men shuffled", menArray);
-// console.log("women shuffled", womenArray);
+/* preload images */
+var preload = {
+  type: jsPsychPreload,
+  images: peopleImages,
+};
+timeline.push(preload);
 
+/* define welcome message trial */
+var welcome = {
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: "Welcome to the experiment. Press any key to begin.",
+};
+timeline.push(welcome);
 
-/**
- *  Initializes men index [mi] & women index [wi] to 0
- *  Chose gender randomly - Gender: 0 = man; 1 = woman
- *  Display the chosen picture
- *  Add one to index for chosen gender
- */
-let mi = 0;
-let wi = 0;
-let gender = Math.round(Math.random());
+/* define instructions trial */
+var instructions = {
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: `
+        <p>In this experiment, a circle will appear in the center 
+        of the screen.</p><p>If the circle is <strong>blue</strong>, 
+        press the letter F on the keyboard as fast as you can.</p>
+        <p>If the circle is <strong>orange</strong>, press the letter J 
+        as fast as you can.</p>
+        <div style='width: 700px;'>
+        <div style='float: left;'><img src='https://www.jspsych.org/latest/img/blue.png'></img>
+        <p class='small'><strong>Press the F key</strong></p></div>
+        <div style='float: right;'><img src='https://www.jspsych.org/latest/img/orange.png'></img>
+        <p class='small'><strong>Press the J key</strong></p></div>
+        </div>
+        <p>Press any key to begin.</p>
+      `,
+  post_trial_gap: 500,
+};
+timeline.push(instructions);
 
-if (gender === 0) {
-  imgSrc = `${PEOPLE_URL}men/man_${menArray[mi]}.jpg`;
-  mi++;
-} else if (gender === 1) {
-  imgSrc = `${PEOPLE_URL}women/woman_${womenArray[wi]}.jpg`;
-  wi++;
-}
+/* define trial stimuli array for timeline variables */
+var test_stimuli = peopleImages.map((imgUrl) => {
+  return {
+    stimulus: imgUrl,
+    correct_response: "f",
+  };
+});
 
-personImg.setAttribute("src", imgSrc);
+/* define fixation and test trials */
+var fixation = {
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: '<div style="font-size:60px;">+</div>',
+  choices: "NO_KEYS",
+  trial_duration: 500,
+  data: {
+    task: "fixation",
+  },
+};
+
+var test = {
+  type: jsPsychImageKeyboardResponse,
+  stimulus: jsPsych.timelineVariable("stimulus"),
+  choices: ["f", "j"],
+  data: {
+    task: "response",
+    correct_response: jsPsych.timelineVariable("correct_response"),
+  },
+  on_finish: function (data) {
+    data.correct = jsPsych.pluginAPI.compareKeys(
+      data.response,
+      data.correct_response
+    );
+  },
+};
+
+/* define test procedure */
+var test_procedure = {
+  timeline: [fixation, test],
+  timeline_variables: test_stimuli,
+  randomize_order: true,
+};
+timeline.push(test_procedure);
+
+/* start the experiment */
+jsPsych.run(timeline);
