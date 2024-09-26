3d990d9e8471c9ad80c959a62069df63cf13b5f8
Sara Martín <saramartinfuentes@hotmail.com>
2024-09-26 11:08:23 +0200

    changes no keys

--- main.js
+++ main.js
@@ -1,107 +1,98 @@
 /**
  * Sara's Experiment
- * Some info...
- *
+ * Facial recognition experiment with automatic image presentation
  */
 
-const PEOPLE_URL =
-  "https://raw.githubusercontent.com/saramff/face-recognition-images/refs/heads/master/";
+const PEOPLE_URL = "https://raw.githubusercontent.com/saramff/face-recognition-images/refs/heads/master/";
 const IMAGES_PER_GENDER = 5;
 
-// Create arrays from picture 1 to IMAGES_PER_GENDER
-const menImages = Array.from(
-  { length: IMAGES_PER_GENDER },
-  (_, i) => `${PEOPLE_URL}/men/man_${i + 1}.jpg`
-);
-const womenImages = Array.from(
-  { length: IMAGES_PER_GENDER },
-  (_, i) => `${PEOPLE_URL}/women/woman_${i + 1}.jpg`
-);
+// Create arrays for men and women images
+const menImages = Array.from({ length: IMAGES_PER_GENDER }, (_, i) => `${PEOPLE_URL}/men/man_${i + 1}.jpg`);
+const womenImages = Array.from({ length: IMAGES_PER_GENDER }, (_, i) => `${PEOPLE_URL}/women/woman_${i + 1}.jpg`);
 const peopleImages = [...menImages, ...womenImages];
 
-/* initialize jsPsych */
+/* Initialize jsPsych */
 let jsPsych = initJsPsych({
   on_finish: function () {
     jsPsych.data.displayData();
   },
 });
 
-/* create timeline */
+/* Create timeline */
 let timeline = [];
 
-/* preload images */
+/* Preload images */
 let preload = {
   type: jsPsychPreload,
   images: peopleImages,
 };
 timeline.push(preload);
 
-/* define welcome message trial */
+/* Welcome message trial */
 let welcome = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: "Welcome to the experiment. Press any key to begin.",
 };
 timeline.push(welcome);
 
-/* define instructions trial */
+/* Instructions trial */
 let instructions = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: `
-    <p>In this experiment, you will see faces appear one by one in the center of the screen.</p>
-    <p>We ask you to pay special attention to each of these faces.</p>
-    <p>They will all pass automatically, one after the other.</p>
-    <p>Press any key to begin.</p>
+    <p>In this experiment, faces will appear one by one automatically.</p>
+    <p>Please pay close attention to each face.</p>
+    <p>Press any key to begin when you're ready.</p>
   `,
   post_trial_gap: 500,
 };
 timeline.push(instructions);
 
-/* define trial stimuli array for timeline variables */
+/* Create stimuli array for image presentation */
 let test_stimuli = peopleImages.map((imgUrl) => {
   return {
-    stimulus: imgUrl
+    stimulus: imgUrl,
   };
 });
 
-/* define fixation and test trials */
+/* Fixation trial */
 let fixation = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: '<div style="font-size:60px;">+</div>',
-  choices: "NO_KEYS",
-  trial_duration: 500,
+  choices: "NO_KEYS",  // Prevent key press
+  trial_duration: 500,  // Fixation duration
   data: {
     task: "fixation",
   },
 };
 
+/* Image presentation trial */
 let test = {
   type: jsPsychImageKeyboardResponse,
   stimulus: jsPsych.timelineVariable("stimulus"),
-  choices: "NO KEYS",
-  trial_duration: 1000
+  choices: "NO_KEYS",  // Prevent key press
+  trial_duration: 1000,  // Display each image for 1 second
 };
 
-/* define test procedure */
+/* Test procedure: fixation + image presentation */
 let test_procedure = {
   timeline: [fixation, test],
   timeline_variables: test_stimuli,
-  randomize_order: true,
+  randomize_order: true,  // Randomize image order
 };
 timeline.push(test_procedure);
 
-/* define instructionsrecognition trial */
+/* Instructions for recognition phase */
 let instructionsrecognition = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: `
     <p>Next, you will see a series of images with an object and a related sentence.</p>
-    <p>You will need to decide whether the sentence is true or false.</p>
-    <p>Press the letter A if it is false, and the letter L if it is true.</p>
-    <p>For example, if the screen shows a picture of Maria and a phone, and the sentence says "Maria has a marker," you should respond false by pressing the A key. If the sentence says "Maria has a phone," you should respond true by pressing the L key.</p>
-    <p>You will now see some practice examples.</p>
+    <p>Press 'A' if the sentence is false, and 'L' if it is true.</p>
+    <p>For example, if the screen shows a picture of Maria and a phone, and the sentence says "Maria has a marker," press 'A'. If it says "Maria has a phone," press 'L'.</p>
+    <p>Press any key to begin the practice examples.</p>
   `,
   post_trial_gap: 500,
 };
 timeline.push(instructionsrecognition);
 
-/* start the experiment */
+/* Run the experiment */
 jsPsych.run(timeline);
