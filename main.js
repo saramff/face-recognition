809e29b05ff180c868d92d4e4cd1ab32a9f26fe2
Sara Martín <saramartinfuentes@hotmail.com>
2024-09-26 20:27:38 +0200

    image+object+suffle added

--- main.js
+++ main.js
@@ -16,11 +16,15 @@ const womenImages = Array.from(
   { length: IMAGES_PER_GENDER },
   (_, i) => `${PEOPLE_URL}/women/woman_${i + 1}.jpg`
 );
+
+// Create new array concatenating men & women images
 const peopleImages = [...menImages, ...womenImages];
 
+// Create name arrays for men and women
 const menNames = ["Paco", "Alberto", "Juan", "Manolo", "Pablo"];
 const womenNames = ["Ana", "María", "Juana", "Sandra", "Sofía"];
 
+// Create suffle function - suffles array index randomly
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
@@ -28,11 +32,13 @@ function shuffle(array) {
   }
 }
 
+// Suffle all arrays (images and names)
 shuffle(menImages);
 shuffle(womenImages);
 shuffle(menNames);
 shuffle(womenNames);
 
+// Create object array for men and women {img, name} 
 const menImgsNames = menImages.map((img, index) => {
   return {
     img: img,
@@ -47,9 +53,39 @@ const womenImgsNames = womenImages.map((img, index) => {
   };
 });
 
+// Create new array concatenating men & women images & names
 const peopleImgsNames = [...menImgsNames, ...womenImgsNames];
 
+// suffle people imgs & names array randomly
 shuffle(peopleImgsNames);
+
+/**********************************************************/
+
+// Create function to get a new array with a random slice from other array
+function getRandomSlice(array, sliceSize) {
+  const arraySlice = [];
+
+  for (let i = 0; i < sliceSize; i++) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    const randomPerson = array.splice(randomIndex, 1)[0];
+    arraySlice.push(randomPerson);
+  }
+
+  return arraySlice;
+}
+
+// Define slice size & create men & women array copy not to alter the original ones
+const SLICE_SIZE = 3;
+const menCopy = [...menImgsNames];
+const womenCopy = [...womenImgsNames];
+
+// Create men & women slice array
+const menSlice = getRandomSlice(menCopy, SLICE_SIZE);
+const womenSlice = getRandomSlice(womenCopy, SLICE_SIZE);
+
+const peopleSlice = [...menSlice, ...womenSlice];
+
+shuffle(peopleSlice);
 
 /* Initialize jsPsych */
 let jsPsych = initJsPsych({
@@ -112,8 +148,8 @@ let fixation = {
 let test = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: jsPsych.timelineVariable("stimulus"),
-  choices: "NO_KEYS",  // Prevent key press
-  trial_duration: 1000,  // Display each image for 1 second
+  choices: "NO_KEYS", // Prevent key press
+  trial_duration: 1000, // Display each image for 1 second
 };
 
 /* Test procedure: fixation + image presentation */
@@ -130,7 +166,8 @@ let instructionsrecognition = {
   stimulus: `
     <p>Next, you will see a series of images with an object and a related sentence.</p>
     <p>Press 'A' if the sentence is false, and 'L' if it is true.</p>
-    <p>For example, if the screen shows a picture of Maria and a phone, and the sentence says "Maria has a marker," press 'A'. If it says "Maria has a phone," press 'L'.</p>
+    <p>For example, if the screen shows a picture of Maria and a phone, and the sentence says "Maria has a marker," press 'A'(NO). 
+    <p>If the screen shows a picture of Maria and a phone and it says "Maria has a phone," press 'L'(YES).</p>
     <p>Press any key to begin the practice examples.</p>
   `,
   post_trial_gap: 500,
